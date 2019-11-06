4cef7d5707cd5e418d57b7de8bd789509cbd2f18
TR <tim@fin0c.net>
2019-11-06 10:58:41 -0500

    Add tests.

---
+++ tests/test_process.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+## file: test_process.py
+## desc: Test functions in process.py.
+
+from dask.distributed import Client
+from dask.distributed import LocalCluster
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import pytest
+
+from src import process
+
+@pytest.fixture(scope='module')
+def root_dir():
+    return Path(__file__).resolve().parent.as_posix()
+
+@pytest.fixture(scope='module')
+def sample_gvf_file(root_dir):
+    return Path(root_dir, 'data/sample-hg38-chromosome-10.gvf').as_posix()
+
+@pytest.fixture(scope='module')
+def sample_gtf_file(root_dir):
+    return Path(root_dir, 'data/sample-hg38-gene-build.gtf').as_posix()
+
+@pytest.fixture(scope='module')
+def client():
+    return Client(LocalCluster(
+        n_workers=1,
+        processes=True,
+        local_directory='/tmp'
+    ))
+
+def test_read_gvf_file(sample_gvf_file):
+    df = process._read_gvf_file(sample_gvf_file)
+
+    assert 'seqid' in df.columns
+    assert 'source' in df.columns
+    assert 'type' in df.columns
+    assert 'start' in df.columns
+    assert 'end' in df.columns
+    assert 'score' in df.columns
+    assert 'strand' in df.columns
+    assert 'phase' in df.columns
+    assert 'attr' in df.columns
+
+    assert len(df.index) == 2
+
+
+def test_read_gtf_file(sample_gtf_file):
+    df = process._read_gtf_file(sample_gtf_file)
+
+    assert 'seqname' in df.columns
+    assert 'source' in df.columns
+    assert 'feature' in df.columns
+    assert 'start' in df.columns
+    assert 'end' in df.columns
+    assert 'score' in df.columns
+    assert 'strand' in df.columns
+    assert 'frame' in df.columns
+    assert 'attr' in df.columns
+
+    assert len(df.index) == 269
+
+
+def test_process_gvf(sample_gvf_file):
+    df = process._read_gvf_file(sample_gvf_file)
+    df = process._process_gvf(df)
+    df = df.compute()
+
+    assert (df.chromosome == 'chr10').all()
+    assert (df.rsid == 1308274876).any()
+    assert (df.rsid == 1486292194).any()
+    assert df[df.rsid == 1308274876].observed.iloc[0] == 'C,G'
+    assert df[df.rsid == 1486292194].observed.iloc[0] == 'G,T'
+    assert (df.maf == 0.0).all()
+    assert df[df.rsid == 1308274876].effect.iloc[0] == 'intergenic'
+    assert df[df.rsid == 1486292194].effect.isin(['intron_variant', 'missense_variant']).all()
+    assert df[df.rsid == 1486292194].transcript.isin(['ENST00000381496', 'ENST00000634311', 'ENST00000280886', 'ENST00000434695']).all()
+    assert len(df.index) == 5
+
+
+def test_process_gtf(sample_gtf_file):
+    df = process._read_gtf_file(sample_gtf_file)
+    df = process._process_gtf(df)
+    df = df.compute()
+
+    assert (df.chromosome == 'chr10').all()
+    assert df.gene_id.isin(['ENSG00000151240', 'ENSG00000201861']).all()
+    assert df.transcript_id.isin(['ENST00000381496', 'ENST00000280886', 'ENST00000634311', 'ENST00000434695', 'ENST00000421992', 'ENST00000364991']).all()
+    assert (df[df.gene_id == 'ENSG00000151240'].biotype == 'protein_coding').all()
+    assert (df[df.gene_id == 'ENSG00000201861'].biotype == 'rRNA_pseudogene').all()
+    assert (df[df.gene_id == 'ENSG00000151240'].gene_name == 'DIP2C').all()
+    assert (df[df.gene_id == 'ENSG00000201861'].gene_name == 'RNA5SP298').all()
+
+    assert len(df.index) == 6
+
+
+def test_process_genes(client, sample_gtf_file):
+    df = process._process_genes(sample_gtf_file)
+    df = df.compute()
+
+    assert (df.chromosome == 'chr10').all()
+    assert df.gene_id.isin(['ENSG00000151240', 'ENSG00000201861']).all()
+    assert df.transcript_id.isin(['ENST00000381496', 'ENST00000280886', 'ENST00000634311', 'ENST00000434695', 'ENST00000421992', 'ENST00000364991']).all()
+    assert (df[df.gene_id == 'ENSG00000151240'].biotype == 'protein_coding').all()
+    assert (df[df.gene_id == 'ENSG00000201861'].biotype == 'rRNA_pseudogene').all()
+    assert (df[df.gene_id == 'ENSG00000151240'].gene_name == 'DIP2C').all()
+    assert (df[df.gene_id == 'ENSG00000201861'].gene_name == 'RNA5SP298').all()
+
+    assert len(df.index) == 6
+
+
+def test_process_variants(client, sample_gvf_file):
+    df = process._process_variants(sample_gvf_file)
+    df = df.compute()
+
+    assert (df.chromosome == 'chr10').all()
+    assert (df.rsid == 1308274876).any()
+    assert (df.rsid == 1486292194).any()
+    assert df[df.rsid == 1308274876].observed.iloc[0] == 'C,G'
+    assert df[df.rsid == 1486292194].observed.iloc[0] == 'G,T'
+    assert (df.maf == 0.0).all()
+    assert df[df.rsid == 1308274876].effect.iloc[0] == 'intergenic'
+    assert df[df.rsid == 1486292194].effect.isin(['intron_variant', 'missense_variant']).all()
+    assert df[df.rsid == 1486292194].transcript.isin(['ENST00000381496', 'ENST00000634311', 'ENST00000280886', 'ENST00000434695']).all()
+    assert len(df.index) == 5
